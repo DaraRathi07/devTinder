70b926f7cc435a3be0d48ab58b976bc4c070dabd
DaraRathi07 <darasinghrathi2421@gmail.com>
2025-09-07 09:57:07 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,45 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req,res) => {
-    res.send("namaste node")
-} );
+// app.use("/" , (req,res) => {
+//     res.send("namaste Dara Singh Rathi")
+// });
 
+
+// app.use("/hello", (req,res) => {
+//     res.send("namaste node")
+// } );
+
+// app.use("/user", (req,res) => {
+//     res.send("HaHaHaHaHa")
+// });
+
+
+
+
+//this will only handle get calls to the /user
+app.get("/user/:userId/:password", (req,res) => {
+    console.log(req.params)
+    res.send({firstname: "Dara", lastname: "Rathi"})
+});
+
+app.post("/user", (req,res) => {
+    res.send("Data sucessfully saved to DB")
+});
+
+
+
+
+
+// this will matcht the all the http methods API calls to /test
 app.use("/test",(req , res) => {
     res.send("Hello fron the server")
 });
+
+
+
+
+
 
 app.listen(3000,() => {
     console.log("server is listening at port 3000")
